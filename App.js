6b43d8ea23a0e8bea36cb968db6e83420195a5b5
abhishek <abmishra700@gmail.com>
2021-10-18 13:49:24 +0530

    add customDrawer

--- App.js
+++ App.js
@@ -1,27 +1,24 @@
-import React from "react";
-import { createStackNavigator } from "@react-navigation/stack";
-import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
 
-import { MainLayout } from './screens'
+import {MainLayout} from './screens';
+import CustomDrawer from './navigation/CustomDrawer';
 
 const Stack = createStackNavigator();
 
 const App = () => {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator
-                screenOptions={{
-                    headerShown: false
-                }}
-                initialRouteName={'Home'}
-            >
-                <Stack.Screen
-                    name="Home"
-                    component={MainLayout}
-                />
-            </Stack.Navigator>
-        </NavigationContainer>
-    )
-}
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+        initialRouteName={'Home'}>
+        <Stack.Screen name="Home" component={CustomDrawer} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
-export default App
+export default App;
---
+++ navigation/CustomDrawer.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {View, Text, Image, TouchableOpacity} from 'react-native';
+
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+} from '@react-navigation/drawer';
+
+import {MainLayout} from '../screens';
+import {COLORS, FONTS, SIZES, constants, icons, dummyData} from '../constants';
+
+const Drawer = createDrawerNavigator();
+
+const CustomDrawer = () => {
+  return (
+    <View style={{flex: 1, backgroundColor: COLORS.primary}}>
+      <Drawer.Navigator
+        screenOptions={{
+          drawerType: 'slide',
+          overlayColor: 'transparent',
+          sceneContainerStyle: {
+            backgroundColor: 'transparent',
+          },
+          drawerStyle: {
+            flex: 1,
+            width: '65%',
+            paddingRight: 20,
+            backgroundColor: 'transparent',
+          },
+        }}
+        initialRouteName="MainLayout">
+        <Drawer.Screen name="MainLayout">
+          {props => <MainLayout {...props} />}
+        </Drawer.Screen>
+      </Drawer.Navigator>
+    </View>
+  );
+};
+
+export default CustomDrawer;
