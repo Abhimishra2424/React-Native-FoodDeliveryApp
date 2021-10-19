235b0547e4b973a628b7efd2a4d9353af20d2f5e
abhishek <abmishra700@gmail.com>
2021-10-19 11:47:28 +0530

    setup  mainLayout

--- screens/MainLayout.js
+++ screens/MainLayout.js
@@ -1,8 +1,28 @@
 import React from 'react';
-import {View, Text} from 'react-native';
-import Animated from 'react-native-reanimated';
+import {
+  View,
+  Text,
+  Image,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  FlatList,
+} from 'react-native';
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+} from 'react-native-reanimated';
+import {connect} from 'react-redux';
+import {setSelectedTab} from '../store/tab/tabActions';
+import {Home, Search, CartTab, Favourite, Notification} from '../screens';
+import {COLORS, icons, SIZES, constants, dummyData, FONTS} from '../constants';
 
-const MainLayout = ({drawerAnimationStyle}) => {
+const MainLayout = ({
+  drawerAnimationStyle,
+  navigation,
+  selectedTab,
+  setSelectedTab,
+}) => {
   return (
     <Animated.View
       style={{
@@ -12,9 +32,27 @@ const MainLayout = ({drawerAnimationStyle}) => {
         backgroundColor: 'white',
         ...drawerAnimationStyle,
       }}>
+      {/* Header */}
+      {/* Content */}
+
       <Text>MainLayout</Text>
+      {/* Footer */}
     </Animated.View>
   );
 };
 
-export default MainLayout;
+function mapStateToProps(state) {
+  return {
+    selectedTab: state.tabReducer.selectedTab,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    setSelectedTab: selectedTab => {
+      return dispatch(setSelectedTab(selectedTab));
+    },
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MainLayout);
